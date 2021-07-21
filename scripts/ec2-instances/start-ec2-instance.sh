7d79819341cf8c7ea9cb1879276a715ef88ee7a2
semura823 <artyom.muraweow2018@yandex.ru>
2021-07-22 02:17:13 +0400

    Add Cloudwatch agent

--- scripts/ec2-instances/start-ec2-instance.sh
+++ scripts/ec2-instances/start-ec2-instance.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-#function deploy {
-#    sudo docker stop petclinic-container
-#    sudo docker rm petclinic-container
-#    sudo docker rmi $(sudo docker images -q 050376771752.dkr.ecr.us-east-1.amazonaws.com/ecr-repo)
-#    aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 050376771752.dkr.ecr.us-east-1.amazonaws.com
-#    latestTag=$(aws ecr list-images --repository-name ecr-repo --query 'imageIds[*].imageTag' --output text --region us-east-1 | python3 latestTag.py)
-#    sudo docker run --name petclinic-container -p 8080:8080 -d 050376771752.dkr.ecr.us-east-1.amazonaws.com/ecr-repo:$latestTag
-#    controlTag=$(aws ecr list-images --repository-name ecr-repo --query 'imageIds[*].imageTag' --output text --region us-east-1 | python3 latestTag.py)
-#    if ! [[ $latestTag == $controlTag ]]; then
-#        deploy
-#    fi
-#}
-
 # Download scripts for instance and unzip
 aws s3 cp s3://191274-scripts-bucket/instance-scripts/instance-scripts.zip ./instance-scripts.zip
 sudo apt -y install unzip
 sudo mkdir /home/ubuntu/instance-scripts
 sudo unzip -o ./instance-scripts.zip -d /home/ubuntu/instance-scripts
 sudo chmod +x /home/ubuntu/instance-scripts/deploy.sh
+
+# Logging
+wget https://s3.us-east-1.amazonaws.com/amazoncloudwatch-agent-us-east-1/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+sudo mkdir -p /usr/share/collectd/
+sudo touch /usr/share/collectd/types.db
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/home/ubuntu/instance-scripts/config.json
 
 # Install python modules
 sudo apt -y install pip
