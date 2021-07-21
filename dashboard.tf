702f916a879efb40ef9297cca95a3d4c8b73b8b7
semura823 <artyom.muraweow2018@yandex.ru>
2021-07-22 02:17:31 +0400

    Add monitoring swap and memory

--- dashboard.tf
+++ dashboard.tf
@@ -44,8 +44,60 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
                 "region": "us-east-1",
                 "title": "Autoscalling group DiskReadBytes"
             }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 14,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/EC2", "NetworkPacketsIn", "AutoScalingGroupName", "petclinic" ],
+                    [ ".", "NetworkPacketsOut", ".", "." ]
+                ],
+                "region": "us-east-1"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 28,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ { "expression": "SEARCH('{CWAgent,AutoScalingGroupName,ImageId,InstanceId,InstanceType} AutoScalingGroupName=\"petclinic\" MetricName=\"mem_used_percent\"', 'Average', 300)", "id": "e1", "period": 300 } ]
+                ],
+                "view": "timeSeries",
+                "stacked": false,
+                "region": "us-east-1",
+                "stat": "Average",
+                "period": 300,
+                "title": "Petclinic MEMORY"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 35,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ { "expression": "SEARCH('{CWAgent,AutoScalingGroupName,ImageId,InstanceId,InstanceType} AutoScalingGroupName=\"petclinic\" MetricName=\"swap_used_percent\"', 'Average', 300)", "id": "e1", "period": 300 } ]
+                ],
+                "unit": "percent",
+                "view": "timeSeries",
+                "stacked": false,
+                "region": "us-east-1",
+                "stat": "Average",
+                "period": 300,
+                "title": "Petclinic SWAP"
+            }
         }
-
     ]
 }
 EOF
